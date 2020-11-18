2cfda57ab099f8f252b1e1ea264e2b861a3556a1
Lasitha Jayawardana <65305702+Lasitha-Jayawardana@users.noreply.github.com>
2020-11-19 02:39:29 +0530

    Add Eye detection & smile detection.

--- Detector.py
+++ Detector.py
@@ -1,8 +1,12 @@
+import numpy as np
 import cv2
 import os
 cascPath = os.path.dirname(
-    cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
-faceCascade = cv2.CascadeClassifier(cascPath)
+    cv2.__file__)
+
+faceCascade = cv2.CascadeClassifier(cascPath + '/data/haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier(cascPath + '/data/haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier(cascPath + '/data/haarcascade_smile.xml')
 video_capture = cv2.VideoCapture(0)
 while True:
     # Capture frame-by-frame
@@ -15,10 +19,34 @@ while True:
                                          flags=cv2.CASCADE_SCALE_IMAGE)
     for (x,y,w,h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h),(0,255,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        
+        eyes = eyeCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor= 1.5,
+            minNeighbors=5,
+            minSize=(5, 5),
+            )
+        
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+            
+        
+        smile = smileCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor= 1.5,
+            minNeighbors=15,
+            minSize=(25, 25),
+            )
+        
+        for (xx, yy, ww, hh) in smile:
+            cv2.rectangle(roi_color, (xx, yy), (xx + ww, yy + hh), (0, 255, 0), 2)
+            
         # Display the resulting frame
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        video_capture.release()
-        cv2.destroyAllWindows()
         break
+video_capture.release()
+cv2.destroyAllWindows()
         
